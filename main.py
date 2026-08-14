d84f9bbb2f39dde968b128fcd9ed021fdfa19869
SaulSaenz483 <saul.saenz.vargas@est.una.ac.cr>
2026-08-14 10:23:37 -0600

    Lab1 commit

---
+++ main.py
@@ -0,0 +1,46 @@
+class nodo:
+    def __init__(self, producto):
+        self.producto = producto
+        self.siguiente = None
+        self.anterior = None
+
+
+class ListaDoblemente():
+    def __init__(self):
+        self.cabeza = None  # Inicio
+        self.cola = None    # Fin
+        self.tam = 0
+
+    def getTamano(self):
+        return self.tam
+
+    def listaVacia(self):
+        return self.cabeza is None
+    def 
+
+
+
+
+
+    if __name__ == "__main__":
+# Crear la lista doblemente enlazada
+    lista = ListaDoblemente()
+    try:
+        with open("datos.txt", "r") as archivo:
+            for linea in archivo:
+                linea = linea.strip()
+                # Evitar líneas vacías
+                if linea != "":
+                    valor = int(linea)
+                    # Insertar el valor en la lista
+                    lista.insertarAlInicio(valor)
+                    lista.imprimirAdelante()
+                    print(f"Cantidad de elementos: {lista.cantidadElementos()}")
+
+    except FileNotFoundError:
+        print("Error: el archivo datos.txt no existe.")
+        exit()
+
+    except ValueError:
+        print("Error: el archivo contiene un dato que no es entero.")
+        exit()
